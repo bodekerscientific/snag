6c02dc0fdb5c3e67d9e39900a59a6f5487c64231
Jono <jono@bodekerscientific.com>
2018-08-31 15:56:28 +1200

    updated for length of input file

--- snag/multiday_sth_ocean.py
+++ snag/multiday_sth_ocean.py
@@ -22,7 +22,7 @@ lon = 165.9375
 num_days = 15  # number of days in input file
 out_dt = 21600  # timestep (s) of input data
 
-datetime_list = make_regular_timeseries(dt.datetime(2000, 1, 1, 12), dt.datetime(2000, 12, 31, 12), 86400)
+datetime_list = make_regular_timeseries(dt.datetime(2000, 1, 1, 13), dt.datetime(2000, 12, 29, 13), 86400)
 
 for ii, i in enumerate(range(0, len(datetime_list), num_days - 1)):
     dt1 = datetime_list[i]
@@ -33,21 +33,21 @@ for ii, i in enumerate(range(0, len(datetime_list), num_days - 1)):
         conf = yaml.load(open(conf))
 
     conf['data'][
-        'filename'] = 'T:/DSC-SCM/scm_input/NZESM/NZESM_nudged_{}days_{}{:02}{:02}_lat{:.3f}_lon{:.3f}.nc'.format(num_days, dt1.year, dt1.month,
-                                                                                                                  dt1.day, lat, lon)
+        'filename'] = 'T:/DSC-SCM/scm_input/NZESM/NZESM_nudged_{}days_{}{:02}{:02}{:04}_lat{:.3f}_lon{:.3f}.nc'.format(num_days, dt1.year, dt1.month,
+                                                                                                                  dt1.day, dt1.hour, lat, lon)
     conf['INDATA']['year_init'] = int(dt1.year)
     conf['INDATA']['month_init'] = int(dt1.month)
     conf['INDATA']['day_init'] = int(dt1.day)
     conf['INDATA']['hour_init'] = 13
     conf['INDATA']['min_init'] = 0
     conf['INDATA']['sec_init'] = 0
-    conf['CNTLSCM']['nfor'] = num_days * (86400 / out_dt)
-    conf['RUNDATA']['ndayin'] = num_days - 1
-    conf['RUNDATA']['nminin'] = (out_dt * ((86400 / out_dt) - 1)) / 60
+    conf['CNTLSCM']['nfor'] = num_days * (86400 / out_dt) + 1
+    conf['RUNDATA']['ndayin'] = num_days
+    conf['RUNDATA']['nminin'] = 0#(out_dt * ((86400 / out_dt) - 1)) / 60
     nc_file = nc.Dataset(conf['data']['filename'])
 
-    # conf['INPROF']['tstari'] = np.round(np.mean(nc_file.variables['surface_temperature'][:]),2)
-    # conf['RUNDATA']['tstar_sea'] = np.round(np.mean(nc_file.variables['surface_temperature'][:]),2)
+    conf['INPROF']['tstari'] = float(np.round(np.mean(nc_file.variables['surface_temperature'][:]),2))
+    conf['RUNDATA']['tstar_sea'] = float(np.round(np.mean(nc_file.variables['surface_temperature'][:]),2))
 
     nl = Namelist(conf)
     # nl.variables['w'][:] = 0
@@ -64,7 +64,7 @@ for ii, i in enumerate(range(0, len(datetime_list), num_days - 1)):
         pass
 
     # dump(nl.as_dict(), stream=open('P:/Projects/DSC-SCM/SNAG/namelist_ARM_MCMURDO_land_{}{:02}{:02}_30min.scm'.format(dt1.year, dt1.month, dt1.day), 'w'))
-    dump(nl.as_dict(), stream=open('P:/Projects/DSC-SCM/SNAG/ensemble_namelists/group04/namelist_STH_OCEAN_{:02}.scm'.format(ii), 'w'))
+    dump(nl.as_dict(), stream=open('P:/Projects/DSC-SCM/SNAG/ensemble_namelists/group05/namelist_STH_OCEAN_{:02}.scm'.format(ii), 'w'))
 # plot
 
 # import matplotlib.pylab as plt
