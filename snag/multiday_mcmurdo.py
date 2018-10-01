294a4fe2bca0d07513024b7fad309b13770ad359
Jono <jono@bodekerscientific.com>
2018-10-02 10:43:26 +1300

    setting land surface to mean of air temperature over period

--- snag/multiday_mcmurdo.py
+++ snag/multiday_mcmurdo.py
@@ -3,6 +3,7 @@
 
 import yaml
 import datetime as dt
+import numpy as np
 from six import string_types
 
 from snag.namelist import Namelist
@@ -12,22 +13,25 @@ from snag.version import version as __version__
 
 from logging import basicConfig
 from utils import make_regular_timeseries
+
 basicConfig()
 
 num_days = 6  # number of days in input file
 out_dt = 1800  # timestep (s) of input data
 
-datetime_list = make_regular_timeseries(dt.datetime(2016, 1, 1), dt.datetime(2016, 1, 15), 86400)
+datetime_list = make_regular_timeseries(dt.datetime(2016, 1, 1), dt.datetime(2017, 1, 3), 86400)
 
-for ii, i in enumerate(range(0,len(datetime_list),num_days-1)):
+for ii, i in enumerate(range(0, len(datetime_list), num_days - 1)):
     dt1 = datetime_list[i]
-
+    print(dt1)
+    # if dt1 == dt.datetime(2016,1,21): # file for 21st was corrupted so just take the period before - now ver2 file exisits
+    #     dt1 = datetime_list[i-num_days+1]
     conf = 'D:/code-GitHub/snag/examples/mcmurdo_land.yml'
 
     if isinstance(conf, string_types):
         conf = yaml.load(open(conf))
 
-    conf['data']['filename'] = 'T:/DSC-SCM/SCM_INPUT/ver2/Blended_ARM_data_{}{:02}{:02}_{}d_{}s.nc'.format(dt1.year, dt1.month, dt1.day,num_days,out_dt)
+    conf['data']['filename'] = 'T:/DSC-SCM/SCM_INPUT/ver2/Blended_ARM_data_{}{:02}{:02}_{}d_{}s.nc'.format(dt1.year, dt1.month, dt1.day, num_days, out_dt)
     conf['INDATA']['year_init'] = int(dt1.year)
     conf['INDATA']['month_init'] = int(dt1.month)
     conf['INDATA']['day_init'] = int(dt1.day)
@@ -38,7 +42,7 @@ for ii, i in enumerate(range(0,len(datetime_list),num_days-1)):
     conf['RUNDATA']['nminin'] = 1410
 
     nl = Namelist(conf)
-    #nl.variables['w'][:] = 0
+    # nl.variables['w'][:] = 0
 
     # set all vertical fluxes to 0 for test case
     nl.config['INPROF']['wi'][:] = 0
@@ -46,13 +50,20 @@ for ii, i in enumerate(range(0,len(datetime_list),num_days-1)):
     nl.config['INOBSFOR']['w_inc'][:] = 0
     nl.config['INOBSFOR']['w_bg'][:] = 0
 
+    # for group 06 set surface temperature and ground temperature to mean of surface air temperature
+    ts_mean = float(np.round(np.mean(nl.config['INOBSFOR']['t_bg'][:, 0]), 2))
+    conf['INPROF']['tstari'] = ts_mean
+    conf['INPROF']['t_deep_soili'] = [ts_mean, ts_mean, ts_mean, ts_mean]
+    conf['RUNDATA']['tstar_land'] = ts_mean
+    conf['INJULES']['tstar_tile'] = [ts_mean, ts_mean, ts_mean, ts_mean, ts_mean, ts_mean, ts_mean, ts_mean, ts_mean]
+
     try:
         nl.validate()
     except:
         pass
 
     # dump(nl.as_dict(), stream=open('P:/Projects/DSC-SCM/SNAG/namelist_ARM_MCMURDO_land_{}{:02}{:02}_30min.scm'.format(dt1.year, dt1.month, dt1.day), 'w'))
-    dump(nl.as_dict(), stream=open('P:/Projects/DSC-SCM/SNAG/ensemble_namelists/group03/namelist_ARM_MCMURDO_land_{:01}.scm'.format(ii), 'w'))
+    dump(nl.as_dict(), stream=open('P:/Projects/DSC-SCM/SNAG/ensemble_namelists/group06/namelist_ARM_MCMURDO_land_{:02}.scm'.format(ii), 'w'))
 # plot
 
 # import matplotlib.pylab as plt
@@ -70,4 +81,3 @@ for ii, i in enumerate(range(0,len(datetime_list),num_days-1)):
 # plt.colorbar()
 #
 # plt.close()
-
